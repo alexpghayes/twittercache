bd58a80024a2a7b9e42bf91b62fc291db713b7d3
alex hayes <alexpghayes@gmail.com>
2020-06-02 20:45:00 -0500

    Export table getters

--- R/cache.R
+++ R/cache.R
@@ -354,6 +354,7 @@ cache_exists <- function() {
   file.exists(get_cache_path())
 }
 
+#' @export
 get_node_table <- function() {
   con <- get_cache_db_connection()
   on.exit(dbDisconnect(con))
@@ -362,6 +363,7 @@ get_node_table <- function() {
 
 # may return duplicated edges. call clean_cache ahead of
 # time to get nicer data
+#' @export
 get_edge_table <- function() {
   con <- get_cache_db_connection()
   on.exit(dbDisconnect(con))
