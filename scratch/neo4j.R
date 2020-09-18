203cb6acf075b5d20d04d14654f7dddb10421ea5
alex hayes <alexpghayes@gmail.com>
2020-09-18 12:12:34 -0600

    Scratch

---
+++ scratch/neo4j.R
@@ -0,0 +1,28 @@
+library(neo4r)
+
+system("docker stop neo4j")
+system("docker rm neo4j")
+system("docker run --name neo4j --env NEO4J_AUTH=neo4j/password --publish=7474:7474 --publish=7687:7687 -d neo4j")
+
+con <- neo4j_api$new(
+  url = "http://localhost:7474",
+  user = "neo4j",
+  password = "password"
+)
+
+con$ping()
+con$get_version()
+con$get_constraints()
+con$get_labels()
+con$get_relationships()
+con$get_index()
+
+str(con)
+
+play_movies() %>%
+  call_neo4j(con)
+
+
+# so for a tabular database, i just need to point to the SQLite file
+
+system("docker stop neo4j && sleep 2 && docker rm neo4j")
