c84c38b4d479926d28444d6d72fd2a0c339cb7e2
alex hayes <alexpghayes@gmail.com>
2020-10-07 15:09:58 -0500

    Move to Github Actions

--- R/cache.R
+++ R/cache.R
@@ -354,20 +354,3 @@ cache_exists <- function() {
   file.exists(get_cache_path())
 }
 
-#' @export
-get_node_table <- function() {
-  con <- get_cache_db_connection()
-  on.exit(dbDisconnect(con))
-  collect(tbl(con, "nodes"))
-}
-
-# may return duplicated edges. call clean_cache ahead of
-# time to get nicer data
-#' @export
-get_edge_table <- function() {
-  con <- get_cache_db_connection()
-  on.exit(dbDisconnect(con))
-
-  collect(tbl(con, "edges"))
-}
-
