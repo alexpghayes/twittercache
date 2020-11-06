c202bad4372f761667f98d5d833b631a1e674d9a
Nathan Kolbow <nkolbow@wisc.edu>
2020-11-06 13:21:53 -0600

    added documentation

--- scratch/new-framework.R
+++ scratch/new-framework.R
@@ -15,6 +15,8 @@ sup4j <- function(query, con) {
   suppressWarnings(call_neo4j(query, con))
 }
 
+n_friendsfollowers = 150
+
 
 #' @param users A character vector of user ids (never screen names)
 #'
@@ -29,20 +31,23 @@ lookup_users <- function(user_ids) {
   new_user_data <- update_users(not_in_graph, lookup=TRUE)
 
   not_sampled <- user_data %>%
-    filter(is.na(sampled_at))
+                 filter(is.na(sampled_at))
   if(dim(not_sampled)[1] == 0)
-    not_sampled <- empty_lookup()
+    upgraded_user_data <- empty_lookup()
   else
-    not_sampled <- not_sampled %>% pull(user_id)
+    upgraded_user_data <- not_sampled %>%
+                          pull(user_id) %>%
+                          update_users(lookup=TRUE)
 
   user_data <- user_data %>%
-    filter(!is.na(sampled_at))
+               filter(!is.na(sampled_at))
 
-  upgraded_user_data <- update_users(not_sampled, lookup=TRUE)
   bind_rows(user_data, new_user_data, upgraded_user_data)
 }
 
 
+#' @return an empty tibble with columns named after all of the lookup_user
+#' properties.  Used when user data is unavailabale
 empty_lookup <- function() {
   tibble(
     user_id='.',
@@ -72,11 +77,17 @@ empty_lookup <- function() {
 }
 
 
+#' @return an empty 2-column tibble used as a placeholder for
+#' when user data is not available
 empty_user_edges <- function() {
   tibble(from='.', to='.') %>% slice(0L)
 }
 
 
+#' @param user_ids list of user_ids to fetch existing lookup_user data for in
+#' the db
+#'
+#' @return a tibble with any existing data for user_ids
 db_lookup_users <- function(user_ids) {
   # return a tibble where each row corresponds to a User and each column
   # to one of the User properties. when a user in not present in the
@@ -87,7 +98,7 @@ db_lookup_users <- function(user_ids) {
   data <- paste('MATCH (n) WHERE n.user_id in ["',
                  paste(user_ids, collapse='","'),
                  '"] RETURN n', sep='') %>%
-    sup4j(con)
+          sup4j(con)
 
   if(length(data) == 0)
     empty_lookup()
@@ -106,7 +117,7 @@ db_lookup_users <- function(user_ids) {
 #' @return The tibble of user data, with one row for each (accessible)
 #' user in `users` and one column for each property of `User` nodes
 #' in the graph database.
-update_users <- function(user_ids, lookup=FALSE, get_friends=FALSE, get_followers=FALSE) {
+update_users <- function(user_ids, sample_size=150, lookup=FALSE, get_friends=FALSE, get_followers=FALSE) {
   # make sure to set sampled_at to Sys.time() and
   # sampled_friends_at and sampled_followers_at to NULL
   # return data on users
@@ -142,30 +153,28 @@ update_users <- function(user_ids, lookup=FALSE, get_friends=FALSE, get_follower
                          ',', sep='')
 
     for(j in seq(1, length(properties))) {
+      if(is.na(info[[properties[j]]]))
+        next
+
       create_node <- paste(create_node, 'n.', properties[j], '=', sep = '')
 
-      if(is.na(info[[properties[j]]])) {
-        create_node <- paste(create_node, "NULL", sep='')
-      } else {
-        if(prop_types[j] == 'chr')
-          create_node <- paste(create_node, '"', info[[properties[j]]], '"', sep = '')
-        else
-          create_node <- paste(create_node, info[[properties[j]]], sep = '')
-      }
-
-      if(j != length(properties))
-        create_node <- paste(create_node, ',', sep = '')
+      if(prop_types[j] == 'chr')
+        create_node <- paste(create_node, '"', info[[properties[j]]], '",', sep = '')
+      else
+        create_node <- paste(create_node, info[[properties[j]]], ',', sep = '')
     }
 
-    new_node <- paste(create_node, ' RETURN n', sep = '') %>%
-      sup4j(con)
+    new_node <- paste(substr(create_node, 1, nchar(create_node)-1), ' RETURN n', sep = '') %>%
+                sup4j(con)
 
     if(length(new_node) != 0) {
       nodes <- nodes %>% bind_rows(new_node$n)
       if(get_friends) {
-        db_connect_friends(info$user_id)
+        db_connect_friends(info$user_id, sample_size)
       }
-      # TODO: ADD GET_FOLLOWERS HERE
+      if(get_followers) {
+        db_connect_followers(info$user_id, sample_size)
+      }
     }
   }
 
@@ -180,8 +189,8 @@ update_users <- function(user_ids, lookup=FALSE, get_friends=FALSE, get_follower
 #'
 #' @return a nx2 tibble where the <from> column is user_id and the <to> column
 #'         is the user_id of user_id's friends
-db_connect_friends <- function(user_id) {
-  friends <- rtweet::get_friends(user_id)
+db_connect_friends <- function(user_id, sample_size) {
+  friends <- rtweet::get_friends(user_id, n=sample_size)
   sup4j(paste('MATCH (n:User {user_id:"', user_id, '"}) SET n.sampled_friends_at="', Sys.time(), '"', sep=''),
         con)
 
@@ -201,8 +210,13 @@ db_connect_friends <- function(user_id) {
 }
 
 
-db_connect_followers <- function(user_id) {
-  friends <- rtweet::get_followers(user_id)
+#' @param user_id a SINGLE user_id to create edges for
+#' @param sample_size total number of followers to fetch for user_id
+#'
+#' @return a 2-column tibble edge list from all of user_id's followers to
+#' user_id
+db_connect_followers <- function(user_id, sample_size) {
+  friends <- rtweet::get_followers(user_id, n=sample_size)
   sup4j(paste('MATCH (n:User {user_id:"', user_id, '"}) SET n.sampled_followers_at="', Sys.time(), '"', sep=''),
         con)
 
@@ -225,7 +239,10 @@ db_connect_followers <- function(user_id) {
 #' Gets the friends for the given user that already exist in the DB.
 #'
 #' @param user_ids a list of user_ids who are already in the DB and
-#'                already have friend edge data
+#' already have friend edge data
+#'
+#' @return a 2-column tibble edge list with entries from the users in user_ids
+#' to their friends
 db_get_friends <- function(user_ids) {
   results <- paste('MATCH (from:User),(to:User) WHERE from.user_id in ["',
                    paste(user_ids, collapse='","'),
@@ -239,11 +256,16 @@ db_get_friends <- function(user_ids) {
 }
 
 
+#' @param user_ids a list of user_ids who are already in the DB and
+#' already have follower edge data
+#'
+#' @return a 2-column tibble edge list with entries from the followers of
+#' the users in user_ids to the users in user_ids
 db_get_followers <- function(user_ids) {
-  get_edges <- paste('MATCH (from:User),(to:User) WHERE to.user_id in ["',
+  results <- paste('MATCH (from:User),(to:User) WHERE to.user_id in ["',
                      paste(user_ids, collapse='","'),
-                     '"] AND (from)-[:FOLLOWS]->(to) RETURN from.user_id, to.user_id', sep='')
-  results <- suppressWarning(call_neo4j(get_edges, con))
+                     '"] AND (from)-[:FOLLOWS]->(to) RETURN from.user_id, to.user_id', sep='') %>%
+             sup4j(con)
 
   if(length(results) != 2)
     return(empty_user_edges())
@@ -256,12 +278,12 @@ db_get_followers <- function(user_ids) {
 #'
 #' @return A tibble where each row corresponds to a follower relationship
 #' from the user in the 'from' column to the user in to 'to' column
-get_followers <- function(user_ids) {
+get_followers <- function(user_ids, sample_size=150) {
   user_ids <- c(user_ids)
   status <- follower_sampling_status(user_ids)
 
-  new_edges <- add_new_followers(status$not_in_graph)
-  upgraded_edges <- add_new_followers(status$sampled_friends_at_is_null)
+  new_edges <- add_new_followers(status$not_in_graph, sample_size)
+  upgraded_edges <- add_new_followers(status$sampled_friends_at_is_null, sample_size)
   existing_edges <- db_get_followers(status$sampled_friends_at_not_null)
 
   bind_rows(new_edges, upgraded_edges, existing_edges)
@@ -272,16 +294,17 @@ get_followers <- function(user_ids) {
 #'
 #' @return A tibble where each row corresponds to a follower relationship
 #' from the user in the 'from' column to the user in to 'to' column
-get_friends <- function(user_ids) {
+get_friends <- function(user_ids, sample_size=150) {
   # here we will need to query twice: once to ask who we actually
   # have *complete* friendship edges for, and then a second time to get
   # those friendship edges
   user_ids <- c(user_ids)
   status <- friend_sampling_status(user_ids)
 
+
   # sample the friends of all the users w/o sampled friends
-  new_edges <- add_new_friends(status$not_in_graph)
-  upgraded_edges <- add_new_friends(status$sampled_friends_at_is_null)
+  new_edges <- add_new_friends(status$not_in_graph, sample_size)
+  upgraded_edges <- add_new_friends(status$sampled_friends_at_is_null, sample_size)
   existing_edges <- db_get_friends(status$sampled_friends_at_not_null)
 
   # need to be careful about duplicate edges here. ideally
@@ -294,7 +317,11 @@ get_friends <- function(user_ids) {
 }
 
 
-add_new_friends <- function(user_ids) {
+#' @param user_ids a list of user_ids to add friend edges to the db for
+#' @param sample_size how many friends to sample at a time for each user
+#'
+#' @return a 2-column tibble edge list from user_ids to their friends
+add_new_friends <- function(user_ids, sample_size) {
   # set sampled_friends_at to Sys.time()
   # sampled_at and sampled_followers_at default to NULL
   # return friends of each user
@@ -314,14 +341,18 @@ add_new_friends <- function(user_ids) {
 }
 
 
-add_new_followers <- function(user_ids) {
+#' @param user_ids a list of user_ids to add follower edges to the db for
+#' @param sample_size how many friends to sample at a time for each user
+#'
+#' @return a 2-column tibble edge list from respective followers to user_ids
+add_new_followers <- function(user_ids, sample_size) {
   # set sampled_followers_at to Sys.time() and
   # sampled_at and sampled_friends_at default to NULL
   # return followers of each user
   user_ids <- c(user_ids)
 
-  if(ia.na(user_ids))
-    return(empty_get_followers())
+  if(is.na(user_ids))
+    return(empty_user_edges())
 
   update_users(user_ids, lookup=FALSE)
 
@@ -334,6 +365,11 @@ add_new_followers <- function(user_ids) {
 }
 
 
+#' @param user_ids to fetch the sampling status for
+#'
+#' @return a list of all users who either (1) are not currently in the
+#' graph, (2) are in the graph but their followers have not been sampled,
+#' (3) are in the graph and have sampled followers
 follower_sampling_status <- function(user_ids) {
   # generate based on queries of user.sampled_followers_at node property
   present_users <- db_lookup_users(user_ids)
@@ -341,6 +377,7 @@ follower_sampling_status <- function(user_ids) {
   unsampled_users <- present_users %>%
     filter(is.na(sampled_followers_at)) %>%
     pull(user_id)
+  sampled_users <- setdiff(user_ids, c(unsampled_users, not_in_graph))
 
   if(length(not_in_graph) == 0)
     not_in_graph <- NA
@@ -357,6 +394,11 @@ follower_sampling_status <- function(user_ids) {
 }
 
 
+#' @param user_ids to fetch the sampling status for
+#'
+#' @return a list of all users who either (1) are not currently in the
+#' graph, (2) are in the graph but their friends have not been sampled,
+#' (3) are in the graph and have sampled friends
 friend_sampling_status <- function(user_ids) {
   # generate based on queries of user.sampled_friends_at node property
   present_users <- db_lookup_users(user_ids)
